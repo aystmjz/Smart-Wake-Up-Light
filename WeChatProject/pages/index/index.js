b5eacd6ba534aaa034fca49d2a72ab74ed86ac8d
aystmjz <2835632114@qq.com>
2025-06-06 01:25:57 +0800

    feat: 微信小程序
    - 新增蓝牙连接页面，实现设备扫描、连接和断开功能
    - 添加设置UUID页面，允许用户自定义服务和特征值UUID
    - 实现数据发送和接收功能，包括温度、湿度等传感器数据
    - 添加设备控制功能，如开关灯、空调等
    - 实现闹钟设置和音乐开关等功能
    - 优化用户界面，增加数据卡片和设置项布局

---
+++ WeChatProject/pages/index/index.js
@@ -0,0 +1,315 @@
+const app = getApp();
+
+Page({
+  data: {
+    device: null, // 蓝牙设备信息
+    connected: false, // 连接状态
+    readyRec: false, // 是否准备接收数据
+    Cmd: 0,
+    Temp: 0, // 温度
+    Hum: 0, // 湿度
+    Led: false, // LED状态
+    Fan: false, // 空调状态
+    sendText: "", // 文本框文本
+    sendData: "", // 发送文本
+    connectState: "未连接", // 当前连接状态
+    deviceadd: "  ", // 设备MAC地址
+    selectedTime: '', // 存储用户选择的时间
+    alarmEnabled: false, // 闹钟开关
+    selectedDays: {
+      mon: false,
+      tue: false,
+      wed: false,
+      thu: false,
+      fri: false,
+      sat: false,
+      sun: false
+    }, // 选择的日期
+    alarmTime: '07:00', // 设置的闹钟时间
+    alarmModes: ['5min', '10min', '20min', '30min', '40min', '60min'], // 闹钟模式选项
+    selectedMode: '5min', // 当前选择的模式
+    hourlyTimeAnnouncement: false, // 整点报时
+    alarmMuzicEnabled: false, // 音乐开关
+  },
+
+  // 页面加载时
+  onShow(options) {
+    if (!app.globalData.refresh) {
+      return;
+    }
+    app.globalData.refresh = false;
+    this.data.connectState = "正在连接";
+    if (this.data.connected) {
+      if (app.globalData.ble_device.deviceId == this.data.device.deviceId) {
+        wx.showToast({
+          title: "已连接",
+          icon: "none",
+        })
+        return;
+      }
+      wx.closeBLEConnection({
+        deviceId: this.data.device.deviceId,
+        success: function (res) {
+          console.log('断开蓝牙设备成功');
+        },
+        fail: function (res) {
+          wx.showToast({
+            title: "断开蓝牙设备失败",
+            icon: "none",
+          })
+          console.log('断开蓝牙设备失败');
+          return;
+        }
+      })
+      this.setData({
+        connected: false,
+      });
+    }
+    // if (this.connected) {
+    //   wx.showToast({
+    //     title: "重新连接请先重启小程序",
+    //     icon: "none",
+    //   })
+    //   return;
+    // }
+    // 获取传递过来的设备信息
+    this.data.device = app.globalData.ble_device;
+    if (!this.data.device || !this.data.device.deviceId) {
+      wx.showToast({
+        title: "设备ID不存在",
+        icon: "none",
+      });
+      return;
+    }
+    const deviceId = this.data.device.deviceId;
+    this.setData({
+      deviceadd: "MAC " + deviceId,
+    });
+    this.connectToDevice(deviceId);
+  },
+
+  // 连接蓝牙设备
+  connectToDevice(deviceId) {
+    wx.createBLEConnection({
+      deviceId,
+      success: (res) => {
+        this.setData({
+          connected: true,
+          connectState: "已连接 " + this.data.device.name,
+        });
+        this.getBLEDeviceServices(deviceId);
+      },
+      fail: (res) => {
+        console.error("蓝牙连接失败", res);
+        this.setData({
+          connected: false,
+          connectState: "连接失败",
+        });
+      },
+    });
+  },
+
+  // 获取蓝牙设备的服务
+  getBLEDeviceServices(deviceId) {
+    wx.getBLEDeviceServices({
+      deviceId,
+      success: (res) => {
+        for (let i = 0; i < res.services.length; i++) {
+          if (res.services[i].uuid === app.globalData.mserviceuuid) {
+            this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid);
+            return;
+          }
+        }
+        this.setData({
+          connectState: "找不到目标服务UUID",
+        });
+      },
+    });
+  },
+
+  // 获取蓝牙设备的特征值
+  getBLEDeviceCharacteristics(deviceId, serviceId) {
+    wx.getBLEDeviceCharacteristics({
+      deviceId,
+      serviceId,
+      success: (res) => {
+        for (let i = 0; i < res.characteristics.length; i++) {
+          const item = res.characteristics[i];
+          if (item.properties.notify) {
+            wx.notifyBLECharacteristicValueChange({
+              deviceId,
+              serviceId,
+              characteristicId: item.uuid,
+              state: true,
+              success: (res) => {
+                console.log("开启通知成功");
+                this.setData({
+                  readyRec: true,
+                });
+              },
+            });
+          }
+        }
+      },
+    });
+    wx.onBLECharacteristicValueChange((characteristic) => {
+      const buf = new Uint8Array(characteristic.value);
+      const recStr = String.fromCharCode.apply(null, buf);
+      console.log("收到数据：", recStr);
+      const data = JSON.parse(recStr);
+      this.setData({
+        Temp: data.Temp,
+        Hum: data.Hum,
+        alarmEnabled: data.Alarm,
+        alarmTime: data.Time,
+        selectedDays: data.Days,
+        selectedMode: data.Mode,
+        hourlyTimeAnnouncement: data.Buzzer,
+        alarmMuzicEnabled: data.Muzic,
+      });
+      wx.showToast({
+        title: "已同步",
+        icon: "none",
+      });
+    });
+  },
+
+  sendData() {
+    if (!this.data.connected) {
+      wx.switchTab({
+        url: '/pages/bluetooth/connect/connect', // 跳转到 index 页面
+      });
+      wx.showToast({
+        title: "请先连接蓝牙设备",
+        icon: "none",
+      });
+      return;
+    }
+
+    this.data.sendData = JSON.stringify({
+      Cmd: this.data.Cmd,
+      Temp: this.data.Temp,
+      Hum: this.data.Hum,
+      Alarm: this.data.alarmEnabled,
+      Time: this.data.alarmTime,
+      Days: this.data.selectedDays,
+      Mode: this.data.selectedMode,
+      Buzzer: this.data.hourlyTimeAnnouncement,
+      Muzic: this.data.alarmMuzicEnabled,
+      Extra: this.data.sendText,
+    })
+
+    const buffer = new ArrayBuffer(this.data.sendData.length);
+    const dataView = new DataView(buffer);
+    for (let i = 0; i < this.data.sendData.length; i++) {
+      dataView.setUint8(i, this.data.sendData.charCodeAt(i));
+    }
+
+    wx.writeBLECharacteristicValue({
+      deviceId: this.data.device.deviceId,
+      serviceId: app.globalData.mserviceuuid,
+      characteristicId: app.globalData.mtxduuid,
+      value: buffer,
+      success: (res) => {
+        wx.showToast({
+          title: "数据发送成功",
+          icon: "none",
+        });
+        console.log("数据发送成功", res);
+        this.data.connected = true;
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: "数据发送失败",
+          icon: "none",
+        });
+        console.error("数据发送失败", res);
+        this.data.connected = false;
+        this.selectComponent()
+      },
+    });
+  },
+
+  // 处理文本输入
+  voteTitle(e) {
+    this.data.sendText = e.detail.value;
+  },
+
+  syncData() {
+    this.data.Cmd = 0;
+    this.sendData();
+  },
+
+  onLedChange(event) {
+    const ledStatus = event.detail.value;
+    this.setData({
+      Led: ledStatus
+    });
+    this.data.Cmd = 1;
+    this.sendData();
+  },
+
+  onFanChange(event) {
+    const fanStatus = event.detail.value;
+    this.setData({
+      Fan: fanStatus
+    });
+    this.data.Cmd = 2;
+    this.sendData();
+  },
+
+  // 时间选择改变时的回调函数
+  onTimeChange(event) {
+    const time = event.detail.value; // 获取用户选择的时间
+    this.setData({
+      selectedTime: time, // 更新数据
+    });
+  },
+
+  // 闹钟开关变化
+  onAlarmSwitchChange(e) {
+    this.setData({
+      alarmEnabled: e.detail.value
+    });
+  },
+
+  // 日期选择变化
+  onWeekdaysChange(e) {
+    const selectedDays = e.detail.value.reduce((acc, day) => {
+      acc[day] = true;
+      return acc;
+    }, {});
+
+    this.setData({
+      selectedDays
+    });
+  },
+
+  // 时间选择变化
+  onTimeChange(e) {
+    this.setData({
+      alarmTime: e.detail.value
+    });
+  },
+
+  // 闹钟模式选择变化
+  onModeChange(e) {
+    this.setData({
+      selectedMode: this.data.alarmModes[e.detail.value]
+    });
+  },
+
+  // 整点报时开关变化
+  hourlyTimeAnnouncement(e) {
+    this.setData({
+      hourlyTimeAnnouncement: e.detail.value
+    });
+  },
+
+  // 音乐开关开关变化
+  onAlarmMuzicChange(e) {
+    this.setData({
+      alarmMuzicEnabled: e.detail.value
+    });
+  }
+});
