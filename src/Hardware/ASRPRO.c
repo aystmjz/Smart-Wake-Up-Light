79b4208a8ec09447f7b918d72ad72e09369e01c8
aystmjz <2835632114@qq.com>
2025-06-05 21:03:45 +0800

    fix: 修复ASRPRO上电时喇叭杂音问题

--- src/Hardware/ASRPRO.c
+++ src/Hardware/ASRPRO.c
@@ -6,35 +6,44 @@ void ASRPRO_Init(void)
 
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     GPIO_SetBits(GPIOB, GPIO_Pin_8);
-    GPIO_SetBits(GPIOB, GPIO_Pin_11);
 }
 
 void ASRPRO_Mute_ON(void)
 {
-    GPIO_ResetBits(GPIOB, GPIO_Pin_11);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    GPIO_SetBits(GPIOB, GPIO_Pin_11);
 }
 
 void ASRPRO_Mute_OFF(void)
 {
-    GPIO_SetBits(GPIOB, GPIO_Pin_11);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
 void ASRPRO_Power_ON(void)
 {
     GPIO_ResetBits(GPIOB, GPIO_Pin_8);
-    Delay_ms(1000);
-    ASRPRO_Mute_ON();
 }
 
 void ASRPRO_Power_OFF(void)
 {
     GPIO_SetBits(GPIOB, GPIO_Pin_8);
-    ASRPRO_Mute_OFF();
 }
 
 void ASRPRO_Power_Control(uint8_t state)
